e70f53eea096dab93c6011ee01c98b95d9c4d6e1
inhye6-6 <haine4546@gmail.com>
2021-10-26 01:35:03 +0900

    [210913] add OCR

--- pre_information.py
+++ pre_information.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 import sqlite3
 
@@ -18,59 +13,49 @@ import time
 
 import facenet
 import detect_align
+import ocr
 
 
-# ### Database
-# - local 사용을 위해 sqlite 사용 
-# - preinfo.db
 
-# #### 함수설명
-# - local_dataset<br> local data를 network에 넣어 나온 feature vector를 database에 stroe하기 위해 processing
-# - store<br> local data를 database에 담음
-
-# In[3]:
-
-
-def local_dataset(local_path):
-    facial_img_paths = []
-    ids=[]
+def local_dataset(local_path, id_):
     instances = []
+    facial_img_path = local_path + id_ + '.jpg'
 
-    for root, directory, files in os.walk(local_path):
-        for file in files:
-            if '.jpg' in file:
-                facial_img_paths.append(root+"/"+file)
-                ids.append(file[:(len(file)-4)])
-    for i in tqdm(range(0, len(facial_img_paths))):
-        facial_img_path = facial_img_paths[i]
-        id= ids[i]
-        #detect and align
-        facial_img = detect_align.preprocess_face(facial_img_path, target_size = (160, 160))
+    # name and birth
+    name, birth = ocr.ocr(facial_img_path)
 
-        #represent
-        embedding = model.predict(facial_img)[0]
+    # detect and align
+    facial_img = detect_align.preprocess_face(facial_img_path, info=True, target_size=(160, 160))
 
-        #store
-        instance = []
-        instance.append(id)
-        instance.append(embedding)
-        instances.append(instance)
+    # represent
+    embedding = model.predict(facial_img)[0]
 
-    df = pd.DataFrame(instances, columns = ["id", "embedding"])
+    # store
+    instance = []
+    instance.append(id_)
+    instance.append(name)
+    instance.append(birth)
+    instance.append(embedding)
+    instances.append(instance)
+
+    df = pd.DataFrame(instances, columns=["id", "name", "birth", "embedding"])
     return df
+
 
 def store(df):
     for index, instance in tqdm(df.iterrows(), total=df.shape[0]):
         ID = instance["id"]
+        name = instance["name"]
+        birth = instance["birth"]
         embeddings = instance["embedding"]
 
-        insert_statement = "INSERT INTO face_meta (ID, EMBEDDING) VALUES (?, ?)"
-        insert_args = (ID, embeddings.tobytes())
+        insert_statement = "INSERT INTO face_meta (ID,name,birth EMBEDDING) VALUES (?, ?, ?, ?)"
+        insert_args = (ID,name,birth, embeddings.tobytes())
         cursor.execute(insert_statement, insert_args)
 
         for i, embedding in enumerate(embeddings):
             insert_statement = "INSERT INTO face_embeddings (FACE_ID, DIMENSION, VALUE) VALUES (?, ?, ?)"
-            insert_args = (index,i, str(embedding))
+            insert_args = (index, i, str(embedding))
             cursor.execute(insert_statement, insert_args)
     conn.commit()
 
@@ -81,47 +66,16 @@ def store(df):
 
 
 if __name__ == "__main__":
-    local_path="/project/img/"
-    model=facenet.loadModel()
-    df=local_dataset(local_path)
+    local_path = "/project/img/"
+    model = facenet.loadModel()
+    df = local_dataset(local_path,'BBB')
     conn = sqlite3.connect('pre_info.db')
     cursor = conn.cursor()
-    """
+
     cursor.execute('''drop table if exists face_meta ''')
     cursor.execute('''drop table if exists face_embeddings''')
-    cursor.execute('''create table face_meta (ID VARCHAR(10) primary key, EMBEDDING BLOB)''')
+    cursor.execute('''create table face_meta (ID VARCHAR(10) primary key, name VARCHAR(10) , birth INT ,EMBEDDING BLOB)''')
     cursor.execute('''create table face_embeddings (FACE_ID INT, DIMENSION INT, VALUE DECIMAL(5, 30))''')
-    """
 
     store(df)
-    
-
-"""
-# #### * Clinent sever에서 가지고 있는 embedding form
-
-# In[6]:
-
-
-    tic = time.time()
-
-    select_statement = "select ID, embedding from face_meta"
-    results = cursor.execute(select_statement)
-
-    instances = []
-    for result in results:
-        img_name = result[0]
-        embedding_bytes = result[1]
-        embedding = np.frombuffer(embedding_bytes, dtype = 'float32')
-
-        instance = []
-        instance.append(img_name)
-        instance.append(embedding)
-        instances.append(instance)
-
-    toc = time.time()
-    print(toc-tic,"seconds")
-
-    db_form = pd.DataFrame(instances, columns = ["ID", "embedding"])
-    print(db_form)
-"""
 
--- verification.py
+++ verification.py
@@ -3,23 +3,20 @@
 
 # In[1]:
 
-
 import sqlite3
 
 import pandas as pd
 import numpy as np
-import os
-from tqdm import tqdm
-import pickle
+
 import matplotlib.pyplot as plt
 from PIL import Image
-import math
 import time
 
 import facenet
 import detect_align
 import cam
 import distance
+
 
 
 # ### 함수 설명
@@ -34,44 +31,54 @@ import distance
 
 
 def get_fv(cam_path):
-    #cam.webcam(cam_path)
+    cam.webcam(cam_path)
     img=detect_align.preprocess_face(cam_path, target_size = (160, 160))
-    target = model.predict(img)[0] 
+    target = model.predict(img)[0]
     return target
+
 
 def load_preinfo(ID):
     tic = time.time()
 
-    select_statement = "select ID, embedding from face_meta where ID = " + "'"+ID+"'"
+    select_statement = "select ID,name, embedding from face_meta where ID = " + "'" + ID + "'"
     results = cursor.execute(select_statement)
     embedding = []
     for result in results:
-        embedding_bytes = result[1]
-        embedding=np.frombuffer(embedding_bytes, dtype = 'float32')
-    
-    return embedding
+        name = result[1]
+        embedding_bytes = result[2]
+        embedding = np.frombuffer(embedding_bytes, dtype='float32')
 
-def verify(ID): 
+    return name,embedding
+
+def verify(ID):
     img_path="/project/img/"+ID+ ".jpg"
     cam_path="/project/cam/"+ID+ ".jpg"
-    source= load_preinfo(ID)
-    target = get_fv(cam_path) 
-    
+    name, source = load_preinfo(ID)
+    target = get_fv(cam_path)
+
     #------------------------------
     #display
-    
+
     fig = plt.figure(figsize = (10, 10))
-    
+
+    img = Image.open(img_path)
+    w, h = img.size
+    h1 = int(h * 0.1)
+    h2 = int(h * 0.7)
+    w1 = int(w * 0.62)
+    w2 = int(w * 0.96)
+    img = img.crop([w1, h1, w2, h2])
+
     ax1 = fig.add_subplot(1,2,1)
     plt.axis('off')
-    plt.imshow(Image.open(img_path))
-    
+    plt.imshow(img)
+
     ax2 = fig.add_subplot(1,2,2)
     plt.axis('off')
     plt.imshow(Image.open(cam_path))
-    
+
     plt.show()
-    
+
     #------------------------------
     #verification
 
@@ -79,7 +86,7 @@ def verify(ID):
     print("=====================")
     print(" VERIFICATION RESULT")
     print("---------------------")
-    
+
     distance.verify(source, target)
     print("===================== \n")
 
@@ -93,6 +100,6 @@ if __name__ == "__main__":
     conn = sqlite3.connect('pre_info.db')
     cursor = conn.cursor()
     model=facenet.loadModel()
-    verify("B611169")
+    verify("BBB")
 
 
